8c76b09551bc93c3bbff1c7c0093f7e30657611b
Ryan Thompson <ryan.thompson@pherocity.com>
2014-03-20 09:57:59 -0600

    adding headless app runner

--- bsri.sh
+++ bsri.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get -y install build-essential git curl wget libqt4-dev
+sudo apt-get -y install build-essential git curl wget libqt4-dev xvfb
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/$USER/.rvm/scripts/rvm
 rvm install 2.1.1
