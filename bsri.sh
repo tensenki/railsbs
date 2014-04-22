a06c3601a9f741cd159c0bae3ae881d832d14e86
tensenki <tensenki@pherocity.com>
2014-04-22 10:47:09 -0600

    Update bsri.sh

--- bsri.sh
+++ bsri.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+bash --login
 sudo apt-get update
 sudo apt-get -y install build-essential git curl wget libqt4-dev xvfb
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/$USER/.rvm/scripts/rvm
 rvm install 2.1.1
+rvm use 2.1.1
+rvm gemset create 2.1.1
+rvm gemset use 2.1.1
 gem install rails
+
 
 
